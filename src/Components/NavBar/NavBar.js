d0b340463324caacc5dab3191946e4697941f630
Lautaro <lautarocarrasco223@gmail.com>
2022-07-20 15:16:13 -0300

    Agregar boostrap y modificar categorias. Emprolijar proyecto

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -10,32 +10,31 @@ const NavBar = ()=>{
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Compra PC</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <Nav.Link href="#link">Contactanos</Nav.Link>
+            <NavDropdown title="Categorias" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">AMD</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
-                Another action
+                Intel
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.3">Nvidia</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Ram
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form className="d-flex">
             <Form.Control
               type="search"
-              placeholder="Search"
+              placeholder="Buscar producto"
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="secondary">Search</Button>
+            <Button variant="secondary">Buscar</Button>
           </Form>
         </Navbar.Collapse>
       </Container>
